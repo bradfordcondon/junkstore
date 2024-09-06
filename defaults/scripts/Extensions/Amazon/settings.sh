e3270aab932141455cba481510729e7a473d6711
Bradford Condon <bradford.condon@gmail.com>
2024-09-06 19:18:37 -0400

    settings and login scripts

--- defaults/scripts/Extensions/Amazon/settings.sh
+++ defaults/scripts/Extensions/Amazon/settings.sh
@@ -2,3 +2,6 @@
 DBNAME="amazon.db"
 # database to use for configs and metadata
 DBFILE="${DECKY_PLUGIN_RUNTIME_DIR}/${DBNAME}"
+
+NILECONF="${DECKY_PLUGIN_DIR}/scripts/nile-config.py"
+export NILE="/bin/flatpak run  com.github.imLinguin.nile"
